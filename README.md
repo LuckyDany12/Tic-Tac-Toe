47d54e3488c65c79d27e1b799b5967cad483fb2e
danielahoruckova <horuckova.d@seznam.czgit config --global user.name>
2023-03-31 21:47:38 +0200

    Tic Tac Toe

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Tic-Tac-Toe
+
+The game is played on a grid that's 3 squares by 3 squares. 
+You are X, your friend is O. Players take turns putting their marks in empty squares. 
+The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner. 
+When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie. 
+
+In my game playes should enter a number (1-9)
+
+```
++---+---+---+
+| 1 | 2 | 3 |
++---+---+---+
+| 4 | 5 | 6 |
++---+---+---+
+| 7 | 8 | 9 |
++---+---+---+
+```
---
+++ tic_tac_toe.py
@@ -0,0 +1,63 @@
+print("Welcome to Tic Tac Toe")
+delimiter = 44* "="
+print(delimiter)
+print("GAME RULES:\n\
+Each player can place one mark (or stone)\n\
+per turn on the 3x3 grid. The WINNER is\n\
+who succeeds in placing three of their marks in a:\n\
+* horizontal,\n\
+* vertical or\n\
+* diagonal row")
+print(delimiter)
+print("Let's start the game")
+print(44 *"-")	
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def create_board():
+    return (
+f"+---+---+---+\n\
+| {board[0]} | {board[1]} | {board[2]} |\n\
++---+---+---+\n\
+| {board[3]} | {board[4]} | {board[5]} |\n\
++---+---+---+\n\
+| {board[6]} | {board[7]} | {board[8]} |\n\
++---+---+---+"
+)
+
+
+print(create_board())
+turn = "O" 
+
+while True:
+    print(delimiter)
+    guess = input(f"Player {turn} | Enter your move number (1-9): ")
+    print(delimiter)
+    if guess.isdigit():
+        index = int(guess) - 1
+        if 0 <= index <= 8 and board[index] == " ":
+            board[index] = board[index].replace(" ", turn)
+            print(create_board())
+            if (board[0] == board[1] == board[2] != " ") or \
+                (board[3] == board[4] == board[5] != " ") or \
+                (board[6] == board[7] == board[8] != " ") or \
+                (board[0] == board[3] == board[6] != " ") or \
+                (board[1] == board[4] == board[7] != " ") or \
+                (board[2] == board[5] == board[8] != " ") or \
+                (board[0] == board[4] == board[8] != " ") or \
+                (board[2] == board[4] == board[6] != " "):
+                print(f"{delimiter}\n\
+Congratulations, the player {turn} WON!\n{delimiter}")
+                break
+            elif " " not in board:
+                print(f"{delimiter}\n\
+The game is a tie!\n{delimiter}")
+                break
+            if turn == "O":
+                turn = "X"
+            else:
+                turn = "O"
+        else:
+            print("Invalid move, please try again.")
+    else:
+        print("Invalid input, please enter a number (1-9).")
